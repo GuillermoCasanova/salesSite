d34ee60f5a52e9a975c4835b20f84147d7aa5712
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2015-06-17 23:24:37 -0400

    - Fixing bug with Shows.js service calling Songkick api not working on mobile. Doing so by using    $http.jsonp insteadof $http.get and including JSON_CALLBACK at the end of the request url

--- client/javascripts/services/shows.js
+++ client/javascripts/services/shows.js
@@ -9,10 +9,7 @@ angular.module('Sales')
 
         getShows : function() {
 
-            return $http({
-                method: 'GET',
-                url: 'http://api.songkick.com/api/3.0/artists/7225704-sales/calendar.json?apikey=u3T0xdtx4AOTUzFw'
-            });
+            return $http.jsonp('http://api.songkick.com/api/3.0/artists/7225704-sales/calendar.json?apikey=u3T0xdtx4AOTUzFw&jsoncallback=JSON_CALLBACK');
         }
 
     }
