5087d8c71d1b747e4b612dfdbafc03eac1e5b1ce
Guillermo Casanova <hi.guillermocasanova@gmail.com>
2015-06-20 00:46:18 -0400

    - Fixing bug with slick carousel breaking angular's bindings due to responsive settings destroying and re-creating the carousel
       - Fixing bug with ssImgProperties directive not applying width and height of image on load, neeed attr.$set to set attribute
    
       - Added more images and edited copy for blog posts on Blog.js service

--- client/javascripts/directives/ssBlogPostscarousel.js
+++ client/javascripts/directives/ssBlogPostscarousel.js
@@ -1,32 +1,32 @@
 angular.module("Sales")
     .directive('ssBlogPostsCarousel', [
-
-        '$window',
         '$sanitize', 
-        function($window) {
+        function() {
 
         return {
 
             replace: true,
             restrict: "E",
-            scope: {}, 
             templateUrl: "templates/directives/ssBlogPostsCarousel.tpl.html",
+            scope: {},
             link: function(scope, element, attrs) {
 
                 $(document).foundation('reveal', 'reflow'); 
 
+
+               scope.selectBlogPost = function(pPost) {
+                   scope.selectedPost = pPost; 
+                   console.log("selected blog post"); 
+                   angular.element("#modal").foundation('reveal', 'open');
+
+                };
             },
             controller: [
             '$scope',
             'Blog',
             function($scope, Blog) {
-                
+                $scope.selectedPost  = {}; 
                 $scope.posts = Blog.getPosts(); 
-
-                $scope.selectBlogPost = function(pPost) {
-                   $scope.selectedPost = pPost; 
-                };
-
             }]
 
 
